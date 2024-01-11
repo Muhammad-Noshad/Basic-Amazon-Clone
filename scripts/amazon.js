be698e4459b48c588b55c8aad5d31d6275143f97
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-01-11 14:04:01 +0500

    Use products.js file for displaying products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,5 @@
 // 1. Save the data
-// 3. Make it interactive
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090   // Not 10.90$ since JS has problem working with floats, so we convert it into cents and after calculation back to dollars
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799  
-}];
+// Saved in data/products.js
 
 // 2. Generate the HTML
 // 2.1 Initialize the HTML
@@ -88,6 +61,6 @@ products.forEach((product) => {
   `;
 });
 
-// 2.2 Put it in the HTML file
+// 2.2 Put it in the HTML file (Make it interactive)
 document.body.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
